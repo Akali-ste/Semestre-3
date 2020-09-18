51841fa856319f9a335a29b04d328036227aa36e
Akali-ste <55484015+Akali-ste@users.noreply.github.com>
2020-09-18 18:06:01 +0200

    V.1

---
+++ bienvenue.py
@@ -0,0 +1,10 @@
+
+"""Bienvenue
+Données : le semestre d’entrée
+Résultat : affiche un message de bienvenue aux étudiant-e-s """
+
+### décl. Et initialiser 
+### séquence d’opérations 
+
+print("Bienvenue !")
+
